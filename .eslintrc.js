d47713d28722238ca19ba6ce1d62dc0fb1666f23
Robin Belon <robin.belon.partner@decathlon.com>
2021-12-22 12:01:35 +0100

    update eslint rules & package

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,10 @@ module.exports = {
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
   ],
   root: true,
   env: {
@@ -20,5 +22,21 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'eol-last': ['error', 'always'],
+    'no-else-return': ['error'],
   },
+  overrides: [
+    {
+      files: ['src/modules/logger/**/*.ts'],
+      rules: {
+        '@typescript-eslint/camelcase': 'off',
+      },
+    },
+    {
+      files: ['*.spec.ts'],
+      rules: {
+        '@typescript-eslint/no-var-requires': 0,
+      },
+    },
+  ],
 };
